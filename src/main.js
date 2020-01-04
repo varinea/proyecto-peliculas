27751dfec8d3f6f868a016c988236b6ed2bbcbae
Corina <corinacvp@gmail.com>
2020-01-04 17:43:19 -0300

    prueba llamando json

--- src/main.js
+++ src/main.js
@@ -1,22 +1,7 @@
 'use strict'
 
-//const search_bar=document.getElementById("search-bar").value;
-document.getElementById('comedy').addEventListener('click', cargarJSON);
 
-function cargarJSON(){
-	fetch('comedy.json')
-	.then(function(response){
-		return response.json();
-	})
-	.then(function(json){
-		let html = '';
-		json.forEach(function(element){
-			html += `
-			<li> ${element.title} ${element.year}</li>
-			`;
-		})
-		document.getElementById('root').innerHTML = html;
-	})
 
-}
-
+fetch('http://www.omdbapi.com/?i=tt3896198&apikey=fbdf5d5c')
+.then(response => response.json())
+.then(json => console.log(json))
