88430858b596517091d4fb761ee9268ea47bb5fc
Corina <corinacvp@gmail.com>
2020-01-04 23:19:29 -0300

    iniciando nav en html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 'use strict'
 
 
+// Star Wars, E.T. El extraterrestre, Metrópolis, Gravity, Jurassic Park
+const arrayMovies = ['tt1618434', 'tt1618434','tt1618434','tt1454468','tt0107290']; 
 
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=fbdf5d5c')
-.then(response => response.json())
-.then(json => console.log(json))
+let arrayData= [];
+arrayMovies.forEach(element => {
+	fetch(`http://www.omdbapi.com/?i=${element}&apikey=fbdf5d5c`)
+	.then(response => response.json())
+	.then(json => arrayData.push(json))
+});
+
+console.log(arrayData);
