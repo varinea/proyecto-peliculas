79031e67e1af32988b7df6e469db606ff9f9f8aa
Corina <corinacvp@gmail.com>
2020-01-05 16:10:16 -0300

    add carpeta img

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 'use strict'
 
 
-// Star Wars, E.T. El extraterrestre, Metrópolis, Gravity, Jurassic Park
-const arrayMovies = ['tt1618434', 'tt1618434','tt1618434','tt1454468','tt0107290']; 
+let searchBar=document.getElementById('search-bar').addEventListener('keydown', (e) => {
+	if (e.keyCode === 13) {
+		fetch(`http://www.omdbapi.com/?s=${e.target.value}&apikey=fbdf5d5c`)
+		  .then(response => response.json())
+		  .then(json => console.log(json))
+	  
+	}
+  });
 
-let arrayData= [];
-arrayMovies.forEach(element => {
-	fetch(`http://www.omdbapi.com/?i=${element}&apikey=fbdf5d5c`)
-	.then(response => response.json())
-	.then(json => arrayData.push(json))
-});
 
-console.log(arrayData);
+// //Ciencia Ficción Star Wars, E.T. El extraterrestre, Metrópolis, Gravity, Jurassic Park
+// //const moviesSciFi = ['tt0076759', 'tt0083866','tt0472646','tt1454468','tt0107290']; 
+
+// let arrayData= [];
+// moviesSciFi.forEach(element => {
+// 	fetch(`http://www.omdbapi.com/?i=${element}&apikey=fbdf5d5c`)
+// 	.then(response => response.json())
+// 	.then(json => arrayData.push(json))
+// });
+// console.log(arrayData);
